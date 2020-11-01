d2619c6507045c781a9de6306e97a20acb8a7ca1
Youngseon Ahn <yoahn@c10r2s7.42seoul.kr>
2020-11-01 15:58:29 +0900

    header file norminette

--- ex00/Include/ft_rush02.h
+++ ex00/Include/ft_rush02.h
@@ -5,26 +5,26 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-typedef struct      s_dict
+typedef struct		s_dict
 {
-    char            *key;
-    char            *value;
-    struct s_dict   *next;
-}                   t_dict;
+	char			*key;
+	char			*value;
+	struct s_dict	*next;
+}					t_dict;
 
-extern t_dict       *g_dict;
-extern int g_fd;
+extern t_dict		*g_dict;
+extern int			g_fd;
 
-t_dict *create_node(char *key, char *val);
-void push_front(char *key, char *val);
-char *get_val(char *key);
+t_dict				*create_node(char *key, char *val);
+void				push_front(char *key, char *val);
+char				*get_val(char *key);
 
-void	ft_units_copy(char *str, char *number_units, int unit_count);
-char	*del_space_left(char *str);
-char	*del_space_right(char *str);
-void insert_key_value(char *str);
-int		is_space(char ch);
+void				ft_units_copy(char *str, char *number_units, int unit_count);
+char				*del_space_left(char *str);
+char				*del_space_right(char *str);
+void				insert_key_value(char *str);
+int					is_space(char ch);
 
-char    *ft_fread(int fd);
-void    ft_open(char *path);
-int ft_is_same(char *s1, char *s2);
+char				*ft_fread(int fd);
+void				ft_open(char *path);
+int					ft_is_same(char *s1, char *s2);
