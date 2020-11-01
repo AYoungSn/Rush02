9e98378f6da6febf15ef6ae6175c75b1499ec01a
Ahn YoungSeon <dudtjs0920@naver.com>
2020-11-01 22:22:00 +0900

    convert

--- ex00/Source/convert.c
+++ ex00/Source/convert.c
@@ -3,84 +3,87 @@
 int g_case3_f;
 int g_case2_f;
 
-void	ft_convert_unit(int index)
+char    ft_str(int cnt)
 {
-	char	*str;
-	char	*lenth;
-	int		i;
+    char    *lenth;
+    int        i;
 
-	if ((g_case3_f == 1) && (index % 3 == 0))
-	{
-		lenth = (char *)malloc(index + 2);
-		lenth[index + 1] =  0;
-		lenth[0] = '1';
-		i = index;
-		while (i > 0)
-			lenth[i--] = '0';
-		str = lenth;
-		ft_print(get_val(str));
-		free(str);
-		g_case3_f = 0;
-	}
-	else if ((g_case2_f == 1) && index % 3 == 2)
-		ft_print(get_val("100"));
+    lenth = (char*)malloc(cnt + 2);
+    lenth[cnt + 1] = 0;
+    lenth[0] = '1';
+    i = cnt;
+    while (i > 0)
+        lenth[i--] = '0';
+    lenthurn (lenth);
 }
 
-int		ft_convert_num(int index, char *num, char *temp)
+void    ft_convert_unit(int index)
 {
-	int lenth;
+    char    *str;
 
-	lenth = 0;
-	if (*num != '0')
-	{
-		g_case3_f = 1;
-		g_case2_f = 1;
-		*temp = *num;
-		if (index % 3 == 2)
-		{   
-			if (*num == '1' && *(num + 1) != '0')
-			{
-				temp[1] = *(num + 1);
-				lenth++;
-			}
-			else
-				temp[1] = '0';
-		}
-		else
-		{
-			temp[1] = 0;
-		}
-		ft_print(get_val(temp));
-	}
-	else
-		g_case2_f = 0;
-	return (lenth);
+    if (g_case3_f && index % 3 == 0)
+    {
+        str = ft_str(index);
+        ft_print(get_val(str));
+        free(str);
+        g_case3_f = 0;
+    }
+    else if (g_case2_f && index % 3 == 2)
+        ft_print(get_val("100"));
 }
 
-
-void	convert(char *num)
+int        ft_convert_num(int index, char *num, char *temp)
 {
-	int		index;
-	int		offset;
-    char	*temp;
-    
-    g_case3_f = 0;
-	g_case2_f = 0;
-	if (num[0] == '0')
-		ft_print(get_val("0"));
-	else
-	{   
-		temp = (char*)malloc(3);
-		temp[2] = 0;
-		index = ft_strlen(num);
-		while (index)
-		{
-			ft_convert_unit(index);
-			offset = ft_convert_num(index, num, temp);
-			num += (1 + offset);
-			index -= (1 + offset);
-		}
-		free(temp);
-	}
-	ft_putstr("\n");
+    int lenth;
+
+    lenth = 0;
+    if (*num != '0')
+    {
+        g_case3_f = 1;
+        g_case2_f = 1;
+        temp[0] = *num;
+        if (index % 3 == 2)
+        {
+            if (*num == '1' && *(num + 1) != '0')
+            {
+                temp[1] = *(num + 1);
+                lenth++;
+            }
+            else
+                temp[1] = '0';
+        }
+        else
+            temp[1] = 0;
+        ft_print(get_val(temp));
+    }
+    else
+        g_case2_f = 0;
+    lenthurn (lenth);
+}
+
+void    convert(char *num)
+{
+    int        index;
+    char    *temp;
+    int        offset;
+
+    if (num[0] == '0')
+        ft_print(get_val("0"));
+    else
+    {
+        temp = (char*)malloc(3);
+        temp[2] = 0;
+        g_case3_f = 0;
+        g_case2_f = 0;
+        index = ft_strlen(num);
+        while (index)
+        {
+            ft_convert_unit(index);
+            offset = ft_convert_num(index, num, temp);
+            num += (1 + offset);
+            index -= (1 + offset);
+        }
+        free(temp);
+    }
+    ft_putstr("\n");
 }
