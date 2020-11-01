6daa82e897e59aaa12896fcf177ff6842a2a1746
AYoungSn <dudtjs0920@naver.com>
2020-11-01 16:04:12 +0900

    헤더파일 주석추가

--- ex00/Include/ft_rush02.h
+++ ex00/Include/ft_rush02.h
@@ -15,16 +15,21 @@ typedef struct		s_dict
 extern t_dict		*g_dict;
 extern int			g_fd;
 
+//linked_list.c
 t_dict				*create_node(char *key, char *val);
 void				push_front(char *key, char *val);
 char				*get_val(char *key);
 
+//dict_split.c
 void				ft_units_copy(char *str, char *number_units, int unit_count);
 char				*del_space_left(char *str);
 char				*del_space_right(char *str);
 void				insert_key_value(char *str);
-int					is_space(char ch);
 
+//ft_io.c
 char				*ft_fread(int fd);
 void				ft_open(char *path);
+
+//valid_str
 int					ft_is_same(char *s1, char *s2);
+int					is_space(char ch);
