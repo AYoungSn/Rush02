3dccfd7510c8bcec32f70b81a3406ec28a816167
Youngseon Ahn <yoahn@c8r4s9.42seoul.kr>
2020-11-01 20:58:47 +0900

    convert.c, main.c

---
+++ ex00/Source/convert.c
@@ -0,0 +1,86 @@
+#include "ft_rush02.h"
+
+int g_case3_f;
+int g_case2_f;
+
+void	ft_convert_unit(int index,int count)
+{
+	char	*str;
+	char	*lenth;
+	int		i;
+
+	if ((g_case3_f == 1) && (index % 3 == 0))
+	{
+		lenth = (char *)malloc(count + 2);
+		lenth[count + 1] =  0;
+		lenth[0] = '1';
+		i = count;
+		while (i > 0)
+			lenth[i--] = '0';
+		str = lenth;
+		ft_print(get_val(str));
+		free(str);
+		g_case3_f = 0;
+	}
+	else if ((g_case2_f == 1) && index % 3 == 2)
+		ft_print(get_val("100"));
+}
+
+int		ft_convert_num(int index, char *num, char *temp)
+{
+	int lenth;
+
+	lenth = 0;
+	if (*num != '0')
+	{
+		g_case3_f = 1;
+		g_case2_f = 1;
+		*temp = *num;
+		if (index % 3 == 2)
+		{   
+			if (*num == '1' && *(num + 1) != '0')
+			{
+				temp[1] = *(num + 1);
+				lenth++;
+			}
+			else
+				temp[1] = '0';
+		}
+		else
+		{
+			temp[1] = 0;
+		}
+		ft_print(get_val(temp));
+	}
+	else
+		g_case2_f = 0;
+	return (lenth);
+}
+
+
+void	convert(char *num)
+{
+	int		index;
+	int		offset;
+    char	*temp;
+    
+    g_case3_f = 0;
+	g_case2_f = 0;
+	if (num[0] == '0')
+		ft_print(get_val("0"));
+	else
+	{   
+		temp = (char*)malloc(3);
+		temp[2] = 0;
+		index = ft_strlen(num);
+		while (index)
+		{
+			ft_convert_unit(index);
+			offset = ft_convert_num(index, num, temp);
+			num += (1 + offset);
+			index -= (1 + offset);
+		}
+		free(temp);
+	}
+	ft_putstr("\n");
+}
--- ex00/Source/main.c
+++ ex00/Source/main.c
@@ -2,9 +2,27 @@
 
 int		main(int argc, char *argv[])
 {
+	char *str;
+
 	g_is_first = 1;
 	g_read_error = 0;
 	g_dict_error = 0;
-	rush(argc, argv);
+	if (argc > 3)
+	{
+		print_error("Error");
+		return (0);
+	}
+	else if (argc == 3)
+	{
+		ft_open(argv[1]);
+		num = argv[2];
+	}
+	else
+	{
+		ft_open("../numbers.dict");
+		num = argv[1];
+	}
+	convert_num(num);
+	free(num);
 	return (0);
 }
