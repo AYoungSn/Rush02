56ee6e22e8b4e53f3da6f2c1e42430e2bf24f3c0
Ahn YoungSeon <dudtjs0920@naver.com>
2020-11-01 17:54:13 +0900

    ft_io.c (fread)

--- ex00/Source/ft_io.c
+++ ex00/Source/ft_io.c
@@ -2,7 +2,28 @@
 
 char	*ft_fread(int fd)
 {
+	int		i;
+	char	*str;
 
+	i = 0;
+	str = (char*)malloc(MAX_SIZE + 1);
+	while (read(fd, str + i, 1) == 1)
+	{
+		//g_eof = 0;
+		if (i == MAX_SIZE)
+		{
+			free(str);
+			error("Error\n");
+			return (0);
+		}
+		if (str[i] == '\n')
+		{
+			str[i] = 0;
+			break ;
+		}
+		i++;
+	}
+	return (str);
 }
 
 void	ft_open(char *path)
